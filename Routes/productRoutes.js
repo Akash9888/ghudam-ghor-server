f26ec441aadef10174f2400e656d22a235b94106
Akash Chanda Tushar <akash.tushar98@gmail.com>
2022-05-04 13:58:09 +0600

    delete and update api created

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../Models/Product");
+const ObjectId = require("mongodb").ObjectId;
 
 const { body, validationResult } = require("express-validator");
 
@@ -43,13 +44,54 @@ router.get("/fetch", async (req, res) => {
     }
 });
 
-//filter products by email
+//find products by email
 
 router.get("/filter/:email", async (req, res) => {
     const products = await Product.find(req.params);
 
     try {
         res.send(products);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+//find product by id
+router.get("/filter/single/:_id", async (req, res) => {
+    console.log(req.params._id);
+    const query = { _id: ObjectId(req.params._id) };
+    console.log(query);
+
+    const products = await Product.find(query);
+    try {
+        res.send(products);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+//update product count
+router.put("/update/:_id", async (req, res) => {
+    const query = { _id: ObjectId(req.params._id) };
+    console.log(query);
+    console.log(req.body);
+
+    try {
+        await Product.findOneAndUpdate(query, req.body, { new: true });
+        await Product.save();
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+//product delete
+router.delete("/delete/:_id", async (req, res) => {
+    const query = { _id: ObjectId(req.params._id) };
+    try {
+        console.log("delete");
+        const result = await Product.findOneAndDelete(query);
+        if (!result) res.status(404).send("No item found");
+        res.status(200).send();
     } catch (e) {
         res.status(500).send(e);
     }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const connectToMongo = require("./db");
+var cors = require("cors");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 connectToMongo();
 
 // app.get("/", (req, res) => {
