38765e99ffd2ccfe77f9b473785db2bcd46b714c
Akash Chanda Tushar <akash.tushar98@gmail.com>
2022-05-04 14:08:45 +0600

    env file resolve

--- db.js
+++ db.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const mongoUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.MOGODB_PASS}@cluster0.33mjx.mongodb.net/gudam-ghor?retryWrites=true&w=majority`;
 
-// const connectToMongo = async () => {
-//     mongoose.connect(mongoUrl, () => {
-//         console.log("Connect to the mongodb server");
-//     });
-// };
 const connectToMongo = async () => {
     try {
         mongoose.connect(
